2f4fdf432586e97c3d3204b4fddc3e1b4b033afe
mkl-74 <mickael1008@gmail.com>
2024-05-21 12:37:32 +0000

    all

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'Light-life','static')));
 
 // Route par défaut - Rediriger vers la page de login
 app.get('/', (req, res) => {
-  res.redirect('./login');
+  res.redirect('/login');
 });
 
 app.post('/create-account', (req, res) => {
@@ -55,12 +55,12 @@ app.post('/create-account', (req, res) => {
 
 // Route pour la page de login
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static','login.html'));
+  res.sendFile(path.join(__dirname, 'Light-life','static','login.html'));
 });
 
 // Route pour la page d'administration
 app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static','admin.html'));
+  res.sendFile(path.join(__dirname, 'Light-life','static','admin.html'));
 });
 
 // Démarrer le serveur
